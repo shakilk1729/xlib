de8d7b654e5d272f22bb569981c13654970f2c87
Shakil Khan <shakilk1729@gmail.com>
2017-07-04 06:42:06 -0700

    After fixing the copyright comments

--- xstring/vector_reverse.cpp
+++ xstring/vector_reverse.cpp
@@ -1,3 +1,4 @@
+/*
 Copyright (C) 2009-2017 Free Software Foundation, Inc.
 
 Written by Shakil A Khan <shakilk1729@gmail.com>
@@ -14,8 +15,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
-
+Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 
 #include <iostream>
 #include <vector>
